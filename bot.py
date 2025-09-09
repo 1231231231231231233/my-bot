de9e2b7ed71be7a9ba89d7775dc0748d29e30f76
1231231231231231233 <mincer632@gmail.com>
2025-09-09 23:49:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,17 +3,20 @@
 # This is a simple bot with schedule timer
 # https://schedule.readthedocs.io
 
-import time, threading, schedule
+import time, threading, schedule, random, math
 from telebot import TeleBot
 
 API_TOKEN = ''
 bot = TeleBot(API_TOKEN)
+balance = 0 
 
-
-@bot.message_handler(commands=['help', 'start'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет!  используй /set и секунды чтобы поставить таймер напиши /unset что бы остонавить таймер, напиши команду /heh и напиши количество чтобы тебе в чат написало текст, напииши /poll чтобы сыграть в квиз")
+    bot.reply_to(message, "Привет! ")
 
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, "/set -таймер \n /unset - выкоючить таймер \n /heh - пишет hehe \n /poll - квиз \n /random - рандомайзер чисел, напиши диапазон через пробел \n /plus, /minus, /divide, /multi - калькулятор напиши 2 числа через пробел и бот сложит/вычтет/разделит/умножит эти 2 числа \n /round - округлит число, напиши дробь и через пробел скольо чисел должно остаться после запитой \n /game - простая игра напиши от одного до пяти чтобы угадать где монета \n /balance - твой баланс ")
 
 def beep(chat_id) -> None:
     """Send the beep message."""
@@ -53,11 +56,56 @@ def create_poll(message):
         is_anonymous=False,
     )
 
-
 @bot.poll_answer_handler()
 def handle_poll(poll):
     pass
 
+@bot.message_handler(commands=['random'])
+def send_random(message):
+    rmsg = message.text.split()
+    rnumber = random.randint(int(rmsg[1]), int(rmsg[2]))
+    bot.reply_to(message, rnumber)
+
+@bot.message_handler(commands=['plus'])
+def send_random(message):
+    smsg = message.text.split()
+    snumber = int(smsg[1]) + int(smsg[2])
+    bot.reply_to(message, snumber)
+
+@bot.message_handler(commands=['divide'])
+def send_divide(message):
+    dmsg = message.text.split()
+    dnumber = int(dmsg[1]) / int(dmsg[2])
+    bot.reply_to(message,dnumber)
+
+@bot.message_handler(commands=['multi'])
+def send_multi(message):
+    mmsg = message.text.split()
+    mnumber = int(mmsg[1]) * int(mmsg[2])
+    bot.reply_to(message, mnumber)
+
+@bot.message_handler(commands=['round'])
+def send_round(message):
+    mmsg = message.text.split()
+    mnumber = round(float(mmsg[1]), int(mmsg[2]))
+    bot.reply_to(message, mnumber)
+
+@bot.message_handler(commands=['game'])
+def send_game(message):
+    global balance
+    pick1 = message.text.split()
+    pick = int(pick1[1])
+    coin = random.randint(1, 5)
+    if pick == coin:
+        bot.reply_to(message, "молодец, ты угадал")
+        balance += 1
+    else:
+        bot.reply_to(message, "увы, но ты проиграл :(")
+
+@bot.message_handler(commands=['balance'])
+def send_balance(message):
+    global balance
+    bot.reply_to(message, balance)
 
 if __name__ == '__main__':
     threading.Thread(target=bot.infinity_polling, name='bot_infinity_polling', daemon=True).start()
